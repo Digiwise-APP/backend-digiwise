3e7f9f803a4d3ee75b19df5823b0709e6bf422dc
Khairun Nisa Cipta Hapsari <nisacipta1413@gmail.com>
2023-09-29 22:50:31 +0800

    change route

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import route from "./src/routes/questionRoute.js";
+import routeLevel from "./src/routes/level.js";
 import { connectDB } from "./src/utils/db.js";
 
 dotenv.config();
@@ -11,7 +12,8 @@ const port = 5000;
 app.use(express.json());
 app.use(cors());
 
-app.use("/", route);
+app.use("/quiz", route);
+app.use("/levels", routeLevel);
 
 connectDB().then(() => {
   app.listen(port, () => {
