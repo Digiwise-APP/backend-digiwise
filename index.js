dca54db1ee4ab1408943dae8dc747ba206ac8362
Khairun Nisa Cipta Hapsari <nisacipta1413@gmail.com>
2023-10-04 13:43:04 +0800

    add route for API user

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
-import route from "./src/routes/questionRoute.js";
+import routeQuestion from "./src/routes/questionRoute.js";
 import routeLevel from "./src/routes/level.js";
+import routeUser from "./src/routes/user.js";
+
 import { connectDB } from "./src/utils/db.js";
 
 dotenv.config();
@@ -12,8 +14,9 @@ const port = 5000;
 app.use(express.json());
 app.use(cors());
 
-app.use("/quiz", route);
+app.use("/quiz", routeQuestion);
 app.use("/levels", routeLevel);
+app.use("/users", routeUser);
 
 connectDB().then(() => {
   app.listen(port, () => {
