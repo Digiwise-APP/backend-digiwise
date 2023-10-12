677ede1b6fb3d39a3610d6ebafaa93e45ac13859
Khairun Nisa Cipta Hapsari <nisacipta1413@gmail.com>
2023-10-12 16:10:15 +0800

    delete file user in all folder

--- src/routes/user.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createUserController, getUserByEmailController } from "../controllers/user.js";
-
-const router = express.Router();
-
-router.post("/", createUserController);
-router.get("/:id", getUserByEmailController);
-
-export default router;
