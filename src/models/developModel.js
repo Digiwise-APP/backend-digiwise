e03670910cb287b521059b569008b60dae1922db
Khairun Nisa Cipta Hapsari <nisacipta1413@gmail.com>
2023-10-22 21:48:34 +0800

    add new model

--- src/models/developModel.js
+++ src/models/developModel.js
@@ -15,7 +15,7 @@ const developUserSchema = new mongoose.Schema(
 
 const developQuestionSchema = new mongoose.Schema(
   {
-    question_type: { type: String, enum: ["PG", "truefalse", "checkbox"], required: true },
+    question_type: { type: String, enum: ["PG", "truefalse", "checkbox", "mix"], required: true },
     level: { type: Number, required: true, enum: [1, 2, 3, 4, 5] },
     question: { type: String, required: true },
     option_answer: { type: String },
@@ -31,7 +31,7 @@ const developUserAnswerSchema = new mongoose.Schema(
   {
     question_id: { type: String, ref: "Question" },
     user_id: { type: String, ref: "User" },
-    question_type: { type: String, enum: ["PG", "truefalse", "checkbox"], required: true },
+    question_type: { type: String, enum: ["PG", "truefalse", "checkbox", "mix"], required: true },
     question: { type: String, required: true },
     option_answer: { type: String },
     level: { type: Number, required: true, enum: [1, 2, 3, 4, 5] },
@@ -45,6 +45,15 @@ const developUserAnswerSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+const developUserAnswerLevelSchema = new mongoose.Schema({
+  user_id: { type: String, ref: "User" },
+  level: { type: Number, required: true, enum: [1, 2, 3, 4, 5] },
+  question_type: { type: String },
+  user_answer: { type: mongoose.Schema.Types.Mixed },
+  attempt: { type: Number },
+  passed: { type: Boolean },
+});
 
 const Userdevelop = mongoose.model("UserDevelop", developUserSchema);
 const Questiondevelop = mongoose.model("QuestionDevelop", developQuestionSchema);
