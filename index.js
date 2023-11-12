bbec896e03b625ee1734172f8bd721ce72fd4e77
Khairun Nisa Cipta Hapsari <nisacipta1413@gmail.com>
2023-11-12 22:18:31 +0800

    use local db

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
-import dotenv from "dotenv";
+import "dotenv/config";
 import express from "express";
+import mongoose from "mongoose";
 import cors from "cors";
 import developRouter from "./src/routes/developRoutes.js";
-import { connectDB } from "./src/utils/db.js";
 import swaggerUi from "swagger-ui-express";
 import swaggerJsdoc from "swagger-jsdoc";
 import apiDocs from "./apidocs.json" assert { type: "json" };
 
-dotenv.config();
+const db = process.env.DATABASE_URL;
+
+mongoose.connect(db);
+const database = mongoose.connection;
 
 const app = express();
 const port = 5000;
+
 app.use(express.json());
 app.use(cors());
+
+//database local
+database.on("error", (error) => {
+  console.log(error, 202020202020);
+});
+
+database.once("connected", () => {
+  console.log("Database Connected");
+});
+
+//develop
+app.use("/", developRouter);
 
 // documentation API with swagger
 const specs = swaggerJsdoc(apiDocs);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
-//develop
-app.use("/", developRouter);
-
-connectDB().then(() => {
-  app.listen(port, () => {
-    console.log(`Server running on port http://localhost:${port}`);
-  });
+app.listen(port, () => {
+  console.log("server listening on part", +port);
 });
