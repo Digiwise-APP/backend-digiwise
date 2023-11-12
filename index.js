42e725b07470ac784309ea54c2a41f42c7050ebe
Khairun Nisa Cipta Hapsari <nisacipta1413@gmail.com>
2023-11-13 00:15:31 +0800

    change

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ import express from "express";
 import cors from "cors";
 import developRouter from "./src/routes/developRoutes.js";
 import { connectDB } from "./src/utils/db.js";
-import swaggerUi from "swagger-ui-express";
-import swaggerJsdoc from "swagger-jsdoc";
-import apiDocs from "./apidocs.json" assert { type: "json" };
 
 dotenv.config();
 
@@ -13,10 +10,6 @@ const app = express();
 const port = 5000;
 app.use(express.json());
 app.use(cors());
-
-// documentation API with swagger
-const specs = swaggerJsdoc(apiDocs);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 //develop
 app.use("/", developRouter);
